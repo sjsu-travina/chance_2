8c27fb71ebf8b0b132c2fa87435a3bd6ae76396f
Trto0191 <travinatorres@gmail.com>
2022-05-23 13:57:08 -0700

    Upload

--- sketch.js
+++ sketch.js
@@ -365,7 +365,7 @@ function tutorialMouseClicked(){
 
 function level1(){
   background(0);
-  imageMode(CENTER);
+  // imageMode(CENTER);
   image(darkImg, w/2, h/2, 600, 600);
   // imageMode(CENTER);
   // image(bckgImg, w/2, h/2, 600, 600);
