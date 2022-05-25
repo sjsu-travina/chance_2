3bc71501b86780b3fb3f1ce3e2f98525468f2054
Trto0191 <travinatorres@gmail.com>
2022-05-24 19:26:12 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -365,6 +365,7 @@ function tutorialMouseClicked(){
 
 function level1(){
   background(0);
+  imageMode(CENTER);
   image(darkImg, w/2, h/2, 600, 600);
   // imageMode(CENTER);
   // imageMode(CENTER);
